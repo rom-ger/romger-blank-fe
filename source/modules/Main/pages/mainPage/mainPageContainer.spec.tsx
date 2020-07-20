a24f17371496dec674ddac00f1bd915d72d5cdee
Romger <r.gerasimovich@white-soft.ru>
2020-07-20 12:08:37 +1000

    тесты

--- source/modules/Main/pages/mainPage/mainPageContainer.spec.tsx
+++ source/modules/Main/pages/mainPage/mainPageContainer.spec.tsx
@@ -2,15 +2,15 @@ import { globalStore } from '@romger/react-global-module/lib/store';
 import { shallow } from 'enzyme';
 import { Provider } from 'mobx-react';
 import * as React from 'react';
-import MainPageContainer, { MainPage, MainPageProps, MainPageState } from './mainPageContainer';
+import MainPageContainer, { MainPage, MainPageInterface, MainPageProps, MainPageState } from './mainPageContainer';
 
 describe('MainPage', () => {
     let mainPageContainer: React.Component<MainPageProps, MainPageState, any> | null = null;
-    let mainPage: React.Component<MainPageProps, MainPageState, any> | null = null;
+    let mainPage: MainPageInterface | null = null;
     // let spy = null;
     const stores = { globalStore };
     // window.scrollTo = () => null;
-    let createInstance = (pathname = '') => shallow(
+    let createInstance: () => MainPageInterface = (pathname = '') => shallow(
         <MainPage
             globalStore={globalStore}
         />,
@@ -37,5 +37,15 @@ describe('MainPage', () => {
     it('can be initialized without an initializer', () => {
         expect(mainPage?.state.userName)
             .toEqual(null);
+    });
+
+    it('getUserName', () => {
+        if (!mainPage) {
+            return;
+        }
+        const spyGetUserName = jest.spyOn(mainPage, 'getUserName');
+        mainPage.componentDidMount();
+        expect(spyGetUserName)
+            .toBeCalled();
     });
 });
